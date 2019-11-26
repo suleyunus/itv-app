853d45e6f8f6cc0af539da9c5c7c6851937c7346
Suleiman Yunus <kayangasuleiman@gmail.com>
2019-11-26 17:08:22 +0300

    feature(password): add password reset and change resources

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const controllers = require('../controllers/users');
+const Auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/auth/creators/signup', controllers.creatorSignup);
 router.post('/auth/login', controllers.login);
+router.post('/auth/password/reset', controllers.passwordReset);
+router.post('/auth/password/reset/done', controllers.passwordResetConfirm);
+router.post('/auth/password/change', Auth.verifyToken, controllers.changePassword);
 
 module.exports = router;
