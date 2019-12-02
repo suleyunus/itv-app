709aa2b746be75218852d082317c0296a0635158
Suleiman Yunus <kayangasuleiman@gmail.com>
2019-12-02 10:13:46 +0300

    feature(groups): add authorisation

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -7,12 +7,12 @@ const router = express.Router();
 router.post('/groups', auth.verifyToken, controllers.createGroup);
 router.get('/groups', auth.verifyToken, controllers.listAllGroups);
 router.get('/groups/:groupId', auth.verifyToken, controllers.getGroupById);
-router.patch('/groups/:groupId', auth.verifyToken, controllers.updateGroupById);
-router.delete('/groups/:groupId', auth.verifyToken, controllers.deleteGroupById);
+router.patch('/groups/:groupId', auth.verifyToken, auth.verifyGroupAdmin, controllers.updateGroupById);
+router.delete('/groups/:groupId', auth.verifyToken, auth.verifyGroupAdmin, controllers.deleteGroupById);
 router.post('/groups/:groupId/members', auth.verifyToken, controllers.joinGroup);
-router.get('/groups/:groupId/members', auth.verifyToken, controllers.listMembersInGroup);
-router.get('/groups/:groupId/members/:memberId', auth.verifyToken, controllers.getMemberInGroup);
+router.get('/groups/:groupId/members', auth.verifyToken, auth.verifyGroupMember, controllers.listMembersInGroup);
+router.get('/groups/:groupId/members/:memberId', auth.verifyToken, auth.verifyGroupMember, controllers.getMemberInGroup);
 // router.update('/group/:groupId/member/:memberId', auth.verifyToken);
-router.delete('/groups/:groupId/members/:memberId', auth.verifyToken, controllers.deleteMemberInGroup);
+router.delete('/groups/:groupId/members/:memberId', auth.verifyToken, auth.verifyOwnerOrAdmin, controllers.deleteMemberInGroup);
 
 module.exports = router;
