25bb965af73070aa4eaa34c1f6b4ae3396a7c114
Suleiman Yunus <kayangasuleiman@gmail.com>
2019-12-05 11:09:41 +0300

    feature(profiles): add user update profile resources

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ const controllers = require('../controllers/users');
 const Auth = require('../middlewares/auth');
 const Validator = require('../middlewares/validator');
 const scheme = require('../util/schemas/users');
+const multerUploads = require('../middlewares/multer');
 
 const router = express.Router();
 
@@ -10,7 +11,10 @@ router.post('/auth/creators/signup', Validator(scheme.creatorSignUpSchema, 'body
 router.post('/auth/login', Validator(scheme.loginSchema, 'body'), controllers.login);
 router.post('/auth/password/reset', Validator(scheme.resetPasswordSchema, 'body'), controllers.passwordReset);
 router.post('/auth/password/reset/done', Validator(scheme.passwordResetDoneSchema, 'body'), controllers.passwordResetConfirm);
-router.post('/auth/password/change', Validator(scheme.changePasswordSchema, 'body'), Auth.verifyToken, controllers.changePassword);
-router.get('/users/:userId', Auth.verifyToken, Validator(scheme.getUserSchema, 'params'), controllers.getUserById);
+router.post('/auth/password/change', Auth.verifyToken, Validator(scheme.changePasswordSchema, 'body'), controllers.changePassword);
+router.get('/users/:userId', Validator(scheme.getUserSchema, 'params'), Auth.verifyToken, controllers.getUserById);
+router.patch('/users/:userId/avatars', Auth.verifyToken, multerUploads.multerUploads, controllers.updateUserAvatar);
+router.delete('/users/:userId/avatars', Auth.verifyToken, multerUploads.multerUploads, controllers.deleteUserAvatar);
+router.patch('/creators/:creatorId', Auth.verifyToken, Validator(scheme.updateCreatorProfileSchema, 'body'), controllers.updateCreatorDetails);
 
 module.exports = router;
